b67cafe25647fbf8db4dd4afff25740cdde9cb54
chsueh2 <chsueh2@ncsu.edu>
2022-06-26 16:27:04 -0400

    R script to render Rmarkdown file

---
+++ markdown render script.R
@@ -0,0 +1,35 @@
+# R script to render Rmarkdown file
+# ----------------------------------------------- 
+
+
+
+# Make sure to have a image folder created
+# Include this in the rmarkdown file to specify the image folder
+# ----------------------------------------------- 
+#
+#```{r include=FALSE}
+#knitr::opts_chunk$set(fig.path = "./images/")
+#```
+#
+# ----------------------------------------------- 
+
+
+
+# input rmarkdown file
+# ----------------------------------------------- 
+input_rmd_file <- ".Rmd"
+
+
+
+# run the following codes to render the file
+# ----------------------------------------------- 
+library(here)
+library(rmarkdown)
+library(knitr)
+
+file <- list.files(pattern='.Rmd')
+rmarkdown::render(
+  rmd_file, 
+  output_format = github_document(html_preview = FALSE), 
+  output_dir = here("README.md")
+)
